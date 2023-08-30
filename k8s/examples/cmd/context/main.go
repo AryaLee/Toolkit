bb13296835c8e43916c1bf49939a8785ca6bac35
AryaLee <meiyun0304@163.com>
2023-08-30 11:33:28 +0800

    add context sample

--- k8s/examples/cmd/context/main.go
+++ k8s/examples/cmd/context/main.go
@@ -8,44 +8,31 @@ import (
 )
 
 func main() {
-	ctx, c_cancel := context.WithCancelCause(context.Background())
-	ctx, cancel := context.WithTimeout(ctx, 3*time.Second)
-	defer cancel()
-
-	go func() {
-		time.Sleep(1 * time.Second)
-		c_cancel(errors.New("test cancel"))
-	}()
-
-	if err := slowProcess(ctx); err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Slow process completed successfully", time.Now())
-	}
-
-	select {
-	case <-time.After(1 * time.Second):
-		fmt.Println("sleep 1 second and process finished", time.Now())
-	case <-ctx.Done():
-		fmt.Println("Process ctx done", time.Now())
-	}
-
-	fmt.Println("ctx error", ctx.Err(), time.Now())
-	fmt.Println("ctx cause", context.Cause(ctx), time.Now())
+	ctx := context.Background()
+	cancelCtx(ctx)
+	fmt.Println("ctx error", ctx.Err())
+	fmt.Println("ctx cause", context.Cause(ctx))
 }
 
-func slowProcess(ctx context.Context) error {
-	// Do some slow work here
-	fmt.Println("begin in process", time.Now())
+func cancelCtx(ctx context.Context) {
+	ctx, cancel := context.WithCancelCause(ctx)
+	defer cancel(nil)
+
+	go sleepFunc(ctx, 1)
+	time.Sleep(3 * time.Second)
+	cancel(errors.New("ctx cancel test"))
+	time.Sleep(5 * time.Second)
+}
+
+func sleepFunc(ctx context.Context, seconds int) {
 	select {
-	case <-time.After(2 * time.Second):
-		return nil
 	case <-ctx.Done():
-		// Check if the context timed out
-		fmt.Println("ctx error in process", ctx.Err(), time.Now())
-		if ctx.Err() == context.DeadlineExceeded {
-			return fmt.Errorf("process timed out")
+		fmt.Println("in sleep func ctx error", ctx.Err())
+		fmt.Println("in sleep func ctx cause", context.Cause(ctx))
+	default:
+		for i := 0; i < 100; i++ {
+			time.Sleep(time.Second * time.Duration(seconds))
+			fmt.Println("time after case", i)
 		}
-		return ctx.Err()
 	}
 }
